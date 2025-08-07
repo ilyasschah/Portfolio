19bc04cec58aaf4185b393ecb94ec93e35cb7d97
ilyass chah <95056514+ilyasschah@users.noreply.github.com>
2025-08-07 22:24:02 +0000

    #test

---
+++ script.js
@@ -0,0 +1,13 @@
+/**
+ * Opens the mobile side navigation panel.
+ */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/**
+ * Closes the mobile side navigation panel.
+ */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
